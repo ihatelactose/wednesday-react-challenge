645e3b3de25dc11b629eeb8c175d06c31df4ea8d
Sourav <sourav@Apples-MacBook-Pro.local>
2022-05-06 10:45:08 +0530

    tests: wrote tests for reducer and sagas for the extra code added - Wrote tests for Reducer, code was added with refactoring async requests to sagas - Wrote tests for Saga, code was added with refactoring async requests to sagas

--- app/containers/ArtistsContainer/tests/saga.test.js
+++ app/containers/ArtistsContainer/tests/saga.test.js
@@ -4,15 +4,17 @@
  *
  */
 import { takeLatest, call, put } from 'redux-saga/effects';
-import artistsContainerSaga, { getArtistsByTerm } from '../saga';
+import artistsContainerSaga, { getArtistsByTerm, getDetailsById } from '../saga';
 import { artistsContainerTypes } from '../reducer';
-import { getArtists } from '@app/services/artistsApi';
+import { getArtists, getDetails } from '@app/services/artistsApi';
 import { apiResponseGenerator } from '@app/utils/testUtils';
 
 describe('ArtistsContainer saga tests', () => {
   const generator = artistsContainerSaga();
   const artistName = 'Stromae';
+  const artistId = 1234;
   let getArtistsGenerator = getArtistsByTerm({ artistsName: artistName });
+  let getDetailsGenerator = getDetailsById({ trackId: artistId });
 
   it('should start task to watch for DEFAULT_ACTION action', () => {
     expect(generator.next().value).toEqual(takeLatest(artistsContainerTypes.REQUEST_GET_ARTISTS, getArtistsByTerm));
@@ -51,6 +53,44 @@ describe('ArtistsContainer saga tests', () => {
     expect(getArtistsGenerator.next(apiResponseGenerator(true, response)).value).toEqual(
       put({
         type: artistsContainerTypes.SUCCESS_GET_ARTISTS,
+        data: response
+      })
+    );
+  });
+
+  it('should dispatch FAILURE_GET_DETAILS once the request fails', () => {
+    const res = getDetailsGenerator.next().value;
+    expect(res).toEqual(call(getDetails, artistId));
+
+    const errorReponse = {
+      errorMessage: "Something went wrong, we're working on it."
+    };
+
+    expect(getDetailsGenerator.next(apiResponseGenerator(false, errorReponse)).value).toEqual(
+      put({
+        type: artistsContainerTypes.FAILURE_GET_DETAILS,
+        error: errorReponse
+      })
+    );
+  });
+
+  it('should dispatch SUCCESS_GET_DETAILS once the request succeeds', () => {
+    getDetailsGenerator = getDetailsById({ trackId: artistId });
+    const res = getDetailsGenerator.next().value;
+    expect(res).toEqual(call(getDetails, artistId));
+
+    const response = {
+      resultCount: 1,
+      results: [
+        {
+          collectionName: 'Multitude'
+        }
+      ]
+    };
+
+    expect(getDetailsGenerator.next(apiResponseGenerator(true, response)).value).toEqual(
+      put({
+        type: artistsContainerTypes.SUCCESS_GET_DETAILS,
         data: response
       })
     );
